59a165db73dcac6b8ef282cb9840419d4694495e
Hamza <Hamza@DESKTOP-JRBJHUE>
2019-07-13 16:26:58 +0200

    Ahmed

---
+++ Test Suites/TS.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>66c6cfcf-3772-44aa-9718-653496f03c26</testSuiteGuid>
+</TestSuiteEntity>
